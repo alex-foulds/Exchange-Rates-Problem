9e91952fc581129d05f3bb3f9d75ca9910264782
Alex Foulds <alex.foulds333@gmail.com>
2022-01-09 21:20:53 +0000

    Added try catch statement to account for error in currencyTo value

--- implementation/exchange-rates.py
+++ implementation/exchange-rates.py
@@ -4,7 +4,7 @@ def main():
 
     exchanges = [["GBP","USD",1.3572132],["GBP","EUR",1.196638],["JPY","USD",0.0086445116]];
     currencyFrom = "EUR";
-    currencyTo = "JPY";
+    currencyTo = "JPN";
 
     exchange = findExchange(exchanges,currencyFrom,currencyTo);
     if exchange:
@@ -14,39 +14,42 @@ def main():
 
 def findExchange(exchanges, currencyFrom, currencyTo, avoidRepeat=None):
     #adding avoidRepeat to skip past troublesome exchanges which leads to recursion error
-    for exchange in exchanges:
+    try:
+        for exchange in exchanges:
 
-        if currencyFrom == exchange[0]:
-            #If the exchange rate exists perfectly in the list
-            if currencyTo == exchange[1]:
-                return exchange[2];
-            
-            #If the exchange exists. Call to see if 'new currency' can exchange to currencyTo
-            elif exchange[1]!=avoidRepeat:
-                return exchange[2]*findExchange(exchanges,exchange[1],currencyTo, exchange[0]);
-            
-            elif exchange[1]==avoidRepeat:
-                continue;
+            if currencyFrom == exchange[0]:
+                #If the exchange rate exists perfectly in the list
+                if currencyTo == exchange[1]:
+                    return exchange[2];
+                
+                #If the exchange exists. Call to see if 'new currency' can exchange to currencyTo
+                elif exchange[1]!=avoidRepeat:
+                    return exchange[2]*findExchange(exchanges,exchange[1],currencyTo, exchange[0]);
+                
+                elif exchange[1]==avoidRepeat:
+                    continue;
 
-            else:
-                #If currency combination is not found
-                return False;
+                else:
+                    #If currency combination is not found
+                    return False;
 
-        elif currencyFrom == exchange[1]:
-            #If the exchange rate exists, but reversed, in the list
-            if currencyTo == exchange[0]:
-                return 1/exchange[2];
+            elif currencyFrom == exchange[1]:
+                #If the exchange rate exists, but reversed, in the list
+                if currencyTo == exchange[0]:
+                    return 1/exchange[2];
 
-            #If the exchange exists reversed. Call to see if 'new currency' can exchange to currencyTo
-            elif exchange[0]!=avoidRepeat:
-                return (1/exchange[2])*findExchange(exchanges,exchange[0], currencyTo, exchange[1]);
-            
-            elif exchange[0]==avoidRepeat:
-                continue;
+                #If the exchange exists reversed. Call to see if 'new currency' can exchange to currencyTo
+                elif exchange[0]!=avoidRepeat:
+                    return (1/exchange[2])*findExchange(exchanges,exchange[0], currencyTo, exchange[1]);
+                
+                elif exchange[0]==avoidRepeat:
+                    continue;
 
-            else:
-                #If currency combination is not found
-                return False;
-
+                else:
+                    #If currency combination is not found
+                    return False;
+    except TypeError as err:
+        print("Unrecognised currency. It does not exist in the exchanges.")
+        return False;
     
 main();
